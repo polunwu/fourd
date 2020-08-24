355d3cf98e9bac5fedb9d8fcc75f8ebe620d0d23
polunwu <polunwu@gmail.com>
2020-08-24 12:35:31 +0800

    Separate result tl file

---
+++ src/pages/result/js/_result_timeline.js
@@ -0,0 +1,59 @@
+import { gsap } from "gsap"
+
+export function registerResultIconTl() {
+  return gsap.timeline({
+    paused: true,
+  })
+    .to('.js-icon-unit', {
+      scale: 1.2,
+      duration: 0.1,
+      repeat: 1,
+      stagger: {
+        each: 0.1,
+        repeat: 1,
+        yoyo: true,
+        from: 'edge',
+        grid: 'auto',
+        axis: 'x'
+      },
+    })
+}
+
+export function registerResultTop75Tl() {
+  return gsap.timeline({
+      paused: true,
+      repeat: 1,
+      repeatDelay: 0.6,
+    })
+    .to('.js-top-1', {
+      fill: '#E9B65A',
+      duration: 0.2,
+    },'0')
+    .to('.js-top-1', {
+      fill: '#176299',
+      duration: 0.4,
+    },'0.8')
+    .to('.js-top-2', {
+      fill: '#E9B65A',
+      duration: 0.2,
+    },'0.2')
+    .to('.js-top-2', {
+      fill: '#176299',
+      duration: 0.4,
+    },'0.8')
+    .to('.js-top-3', {
+      fill: '#E9B65A',
+      duration: 0.2,
+    },'0.4')
+    .to('.js-top-3', {
+      fill: '#176299',
+      duration: 0.4,
+    },'0.8')
+    .to('.js-top-4', {
+      fill: '#ee664e',
+      duration: 0.2,
+    },'0.6')
+    .to('.js-top-light', {
+      opacity: 1,
+    }, '0.8')
+}
--- src/pages/result/js/_script.js
+++ src/pages/result/js/_script.js
@@ -1,5 +1,6 @@
 import { gsap } from "gsap"
 import { getIconUnit } from "./_icon_unit.js";
+import { registerResultIconTl, registerResultTop75Tl } from "./_result_timeline.js";
 
 window.resultData = {
   timeUnit: {
@@ -38,56 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('load', () => {
   
   if (result.type === 'top') {
-    gsap.timeline({
-      repeat: -1,
-      repeatDelay: 0.6,
-    })
-    .to('.js-top-1', {
-      fill: '#E9B65A',
-      duration: 0.2,
-    },'0')
-    .to('.js-top-1', {
-      fill: '#176299',
-      duration: 0.4,
-    },'0.8')
-    .to('.js-top-2', {
-      fill: '#E9B65A',
-      duration: 0.2,
-    },'0.2')
-    .to('.js-top-2', {
-      fill: '#176299',
-      duration: 0.4,
-    },'0.8')
-    .to('.js-top-3', {
-      fill: '#E9B65A',
-      duration: 0.2,
-    },'0.4')
-    .to('.js-top-3', {
-      fill: '#176299',
-      duration: 0.4,
-    },'0.8')
-    .to('.js-top-4', {
-      fill: '#ee664e',
-      duration: 0.2,
-    },'0.6')
-    .to('.js-top-light', {
-      opacity: 1,
-    }, '0.8')
+    let resultTop75Tl = registerResultTop75Tl()
+    resultTop75Tl.play()
   } else {
-    gsap.timeline()
-    .to('.js-icon-unit', {
-      scale: 1.2,
-      duration: 0.1,
-      repeat: 1,
-      stagger: {
-        each: 0.1,
-        repeat: 1,
-        yoyo: true,
-        from: 'edge',
-        grid: 'auto',
-        axis: 'x'
-      },
-    })
+    let resultIconTl = registerResultIconTl()
+    resultIconTl.play()
   }
 })
 
